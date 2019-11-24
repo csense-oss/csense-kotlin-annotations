5c4404e902e5edab8032e2d2f7e095b8b3a73206
Kasper Tvede <kasper@webmasteren.eu>
2019-11-24 16:58:14 +0100

    Add InAny
    since the contexts can create some situations where we want to "opt out" and say "this can be called from anywhere" we have to have that (a "InAny" context)

---
+++ src/commonMain/kotlin/csense/kotlin/annotations/threading/InAny.kt
@@ -0,0 +1,15 @@
+package csense.kotlin.annotations.threading
+
+/**
+ * The concept with this is to allow certain constructs to exists that can work in any context (ui or background alike)
+ * this could be constructs that changes the threading model to say UI.
+ *
+ */
+@Target(
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.PROPERTY_SETTER,
+    AnnotationTarget.PROPERTY_GETTER,
+    AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.BINARY) //binary so its persisted across libraries
+@MustBeDocumented
+annotation class InAny
