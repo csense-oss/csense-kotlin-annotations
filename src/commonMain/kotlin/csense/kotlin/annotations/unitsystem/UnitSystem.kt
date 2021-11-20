e23c7295334d3b563d3d25c356587db62d1484e8
Kasper T <kasper@webmasteren.eu>
2021-11-20 16:06:55 +0100

    dont double comment

--- src/commonMain/kotlin/csense/kotlin/annotations/unitsystem/UnitSystem.kt
+++ src/commonMain/kotlin/csense/kotlin/annotations/unitsystem/UnitSystem.kt
@@ -2,15 +2,15 @@
 
 package csense.kotlin.annotations.unitsystem
 
-///**
-// * idea:
-// * user declares ValueTypes (as an object) with const val's which are strings (or it might be a enum or alike, or sealed class with string value)
-// * @ValueType(ValueTypes.PX)
-// * or "UnitType"
-// * or UnitSystem
-// *
-// */
-//
+/**
+ * idea:
+ * user declares ValueTypes (as an object) with const val's which are strings (or it might be a enum or alike, or sealed class with string value)
+ * @ValueType(ValueTypes.PX)
+ * or "UnitType"
+ * or UnitSystem
+ *
+ */
+
 //@Target(
 //    AnnotationTarget.FUNCTION,
 //    AnnotationTarget.PROPERTY,
