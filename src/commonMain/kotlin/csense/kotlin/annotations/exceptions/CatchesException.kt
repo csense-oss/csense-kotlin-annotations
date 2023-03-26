53534b9c177320ce71719d9dc78ee2a2ea8eeeb1
Kasper T <kasper@webmasteren.eu>
2023-03-26 19:56:04 +0200

    bump and improve annotation(s)

--- src/commonMain/kotlin/csense/kotlin/annotations/exceptions/CatchesException.kt
+++ src/commonMain/kotlin/csense/kotlin/annotations/exceptions/CatchesException.kt
@@ -4,18 +4,14 @@ import kotlin.reflect.*
 
 /**
  * Denotes that a given lambda parameter's exception(of type T) will be caught.
- * This goes in hand with the kotlin checked exception plugin. 
+ * This goes in hand with the kotlin checked exception plugin.
  */
 @Target(
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY,
-    AnnotationTarget.PROPERTY_SETTER,
-    AnnotationTarget.PROPERTY_GETTER,
     AnnotationTarget.VALUE_PARAMETER,
     AnnotationTarget.TYPE //for functional types
 )
 @Retention(AnnotationRetention.BINARY)
 @MustBeDocumented
-public annotation class CatchesException<T : Exception>(
-    val clazz: KClass<T>
+public annotation class CatchesExceptions(
+    val type: KClass<out Throwable> = Throwable::class
 )
