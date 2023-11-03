81a9ad52471e5cceb773779dc0702bbbfdb51761
Kasper T <kasper@webmasteren.eu>
2023-11-03 21:34:17 +0100

    most of the initial setup for a "unit system" annotation

--- src/commonMain/kotlin/csense/kotlin/annotations/unitsystem/UnitSystem.kt
+++ src/commonMain/kotlin/csense/kotlin/annotations/unitsystem/UnitSystem.kt
@@ -1,25 +1,29 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "UNUSED_VARIABLE", "UNUSED_PARAMETER")
 
 package csense.kotlin.annotations.unitsystem
 
+import kotlin.reflect.KClass
+
 /**
- * idea:
- * user declares ValueTypes (as an object) with const val's which are strings (or it might be an enum or alike, or sealed class with string value)
- * @ValueType(ValueTypes.PX)
- * or "UnitType"
- * or UnitSystem
+ * Used to start a new "unit" system
+ * If marked on say a sealed interface with annotation "sub" classes (inside), they would be considered part of the unit system
  *
  */
+@Target(AnnotationTarget.CLASS)
+@Retention(AnnotationRetention.BINARY)
+@MustBeDocumented
+public annotation class UnitSystemOf(
+    val shouldWarnOnMissingUnits: Boolean = false,
+    val shouldWarnOnMismatchingUnitsInGeneral: Boolean = true,
+    val shouldMarkOccurrencesWithUnits: Boolean = false //TODO?
+)
 
-//@Target(
-//    AnnotationTarget.FUNCTION,
-//    AnnotationTarget.PROPERTY,
-//    AnnotationTarget.PROPERTY_SETTER,
-//    AnnotationTarget.PROPERTY_GETTER,
-//    AnnotationTarget.VALUE_PARAMETER
-//)
-//@Retention(AnnotationRetention.BINARY) 
-//@MustBeDocumented
-//public annotation class UnitSystem(
-//    val unitName: String,
-//)
+/**
+ * Adds the annotated class to the given [kClass] UnitSystem (requires the [kClass] to be annotated with [UnitSystemOf])
+ */
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.BINARY)
+@MustBeDocumented
+public annotation class UnitTypeOf(
+    val kClass: KClass<out Any>
+)
